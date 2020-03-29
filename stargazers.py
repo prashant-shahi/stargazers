7440ad48346ec3544f45c21659958098b12b3b87
Prashant Shahi <prashant@dgraph.io>
2020-03-30 03:58:45 +0530

    fixes isseus and code cleanup

--- stargazers.py
+++ stargazers.py
@@ -2,9 +2,10 @@
 
 import argparse
 import csv
+import json
 import math
-import os
 import requests
+import sys
 
 GITHUB_API_URL = "https://api.github.com"
 
@@ -17,8 +18,9 @@ parser.add_argument("--output-type", type=str, choices=["csv", "json"], help="ou
 parser.add_argument("-o", "--out-file", type=str, default="stargazers", help="name of the output file (default: stargazers)")
 
 args = parser.parse_args()
-out_type = args["output-type"]
-out_file = args["output-file"]
+print(args)
+output_type = args.output_type
+out_file = args.out_file
 
 def get_stargazers_count(owner, repo):
     response = get_request("{0}/repos/{1}/{2}".format(GITHUB_API_URL, owner, repo))
@@ -30,35 +32,75 @@ def get_request(url, auth=None, headers=None, params=None):
     if response.status_code != 200:
         print("Non-200 status code received: {0}".format(response_json["message"]))
         print("Ensure that the passed parameters are valid and try again.")
-        os.exit(1)
+        sys.exit(1)
     return response_json
 
 def get_stargazers(owner, repo, username, token):
     total_stars = get_stargazers_count(owner, repo)
-    total_requests = math.ceil(total_start / 30)
+    total_requests = math.ceil(total_stars / 30)
     auth = (username, token)
-    headers = { "Accept" "application/vnd.github.v3+json" }
+    headers = { "Accept": "application/vnd.github.v3+json" }
     if output_type == "json":
         out_json=[]
     if output_type == "csv":
-        csv_writer = csv.writer(open("{0}.csv".format(out_file), "wb+"))
-        csv_writer.writerow(["login","id","node_id","avatar_url","gravatar_id","url","html_url","followers_url","following_url","gists_url","starred_url","subscriptions_url","organizations_url","repos_url","events_url","received_events_url","type","site_admin"])
+        csv_writer = csv.writer(open("{0}.csv".format(out_file), "w"))
+        csv_writer.writerow([
+            "login",
+            "id",
+            "node_id",
+            "avatar_url",
+            "gravatar_id",
+            "url","html_url",
+            "followers_url",
+            "following_url",
+            "gists_url",
+            "starred_url",
+            "subscriptions_url",
+            "organizations_url",
+            "repos_url","events_url",
+            "received_events_url",
+            "type",
+            "site_admin"
+        ])
     for i in range(1, total_requests+1):
-        response = get_request("{0}/repos/{1}/{2}/stargazers", auth=auth, headers=headers, params={ "page": str(i) })
-        if output_type == "json":
-            merge_dicts(out_json, response)
-        elif output_type == "csv":
-            csv_writer.writerow([response["login"], response["id"], response["node_id"], response["avatar_url"], response["gravatar_id"], response["url"], response["html_url"], response["followers_url"], response["following_url"], response["gists_url"], response["starred_url"], response["subscriptions_url"], response["organizations_url"], response["repos_url"], response["events_url"], response["received_events_url"], response["type"], response["site_admin"]])
+        response = get_request(
+            "{0}/repos/{1}/{2}/stargazers".format(GITHUB_API_URL, owner, repo),
+            auth=auth,
+            headers=headers,
+            params={ "page": str(i) }
+        )
+        for user in response:
+            if output_type == "json":
+                out_json.append(user)
+            elif output_type == "csv":
+                csv_writer.writerow([
+                    user["login"],
+                    user["login"],
+                    user["id"],
+                    user["node_id"],
+                    user["avatar_url"],
+                    user["gravatar_id"],
+                    user["url"],
+                    user["html_url"],
+                    user["followers_url"],
+                    user["following_url"],
+                    user["gists_url"],
+                    user["starred_url"],
+                    user["subscriptions_url"],
+                    user["organizations_url"],
+                    user["repos_url"],
+                    user["events_url"],
+                    user["received_events_url"],
+                    user["type"],
+                    user["site_admin"]
+                ])
     if output_type == "json":
-        with open(output_file, 'w') as outfile:
-            json.dump(data, outfile)
+        with open(out_file, 'w') as outfile:
+            json.dump(out_json, outfile)
     elif output_type != "csv":
         print(out_json)
+    print("{0} DONE {0}".format("*"*5))
 
 if __name__ == "__main__":
-    print(args.owner)
-    print(args.repo)
-    print(args.username)
-    print(args.token)
-    print(args["output-type"])
-    print(args["out-file"])
+    get_stargazers(args.owner, args.repo, args.username, args.token)
+
